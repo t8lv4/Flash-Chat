f474828d4912de2e4140dec10830a615e72948ef
t8lv4 <morgan@t8lv4.fr>
2018-06-11 21:46:21 +0200

    Connect app to Database

--- Flash-Chat/AppDelegate.swift
+++ Flash-Chat/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //TODO: Initialise and Configure your Firebase here:
         FirebaseApp.configure()
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("Data! yeay !")
         
         return true
     }
