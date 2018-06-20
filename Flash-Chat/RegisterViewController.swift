845f1ecf5f71ef668cbd6e32801fc1f3ab68cebf
t8lv4 <morgan@t8lv4.fr>
2018-06-20 19:04:41 +0200

    Let user authenticate with email
    create createUser(withEmail:password:)
    create performSegue(withIdentifier:sender:) to go to chat scene

--- Flash-Chat/RegisterViewController.swift
+++ Flash-Chat/RegisterViewController.swift
@@ -6,13 +6,13 @@
 //
 
 import UIKit
-
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     
     //Pre-linked IBOutlets
-
+    
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
@@ -20,24 +20,23 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
-  
-    @IBAction func registerPressed(_ sender: AnyObject) {
-        
-
-        
-        //TODO: Set up a new user on our Firbase database
-        
-        
-
-        
-        
-    } 
     
+    
+    @IBAction func registerPressed(_ sender: AnyObject) {
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("registration succeed")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
+    } 
     
 }
