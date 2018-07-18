741c6f49579d5fcd1e0f06500df9d2677522ffd9
t8lv4 <morgan@t8lv4.fr>
2018-07-18 16:40:36 +0200

    Create func retrieveMessage()
    create messageArray : [Message] to populate tableView cells

--- Flash-Chat/AppDelegate.swift
+++ Flash-Chat/AppDelegate.swift
@@ -19,8 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //TODO: Initialise and Configure your Firebase here:
         FirebaseApp.configure()
-        let myDatabase = Database.database().reference()
-        myDatabase.setValue("Data! yeay !")
         
         return true
     }
